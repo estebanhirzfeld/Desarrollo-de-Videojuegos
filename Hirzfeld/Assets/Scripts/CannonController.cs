a2dbe2dd875b0a9ed14d4ca07aa43017a406fc39
estebanhirzfeld <estebanhirzfeld@gmail.com>
2021-10-27 18:20:16 -0300

    Disparos temporizados

--- Hirzfeld/Assets/Scripts/CannonController.cs
+++ Hirzfeld/Assets/Scripts/CannonController.cs
@@ -5,22 +5,23 @@ using UnityEngine;
 public class CannonController : MonoBehaviour
 {
     public GameObject bulletPrefab;
+    public float fireFrequency = 10f;
+
     // Start is called before the first frame update
     void Start()
     {
+        InvokeRepeating("SpawnBullet" ,1f, fireFrequency);   //Repeticion de Disparo con frecuencia variable desd Inspector
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))                //Con El Click Izquierdo El cannon Dispara una bola de Tennis
-        {
-            Debug.Log("Disparo!");
-            Instantiate(bulletPrefab, transform);
-        }
+        
     }
 
-
-
+    void SpawnBullet()                                          //Funcion para instanciar el prefab de la bala
+    {
+        Instantiate(bulletPrefab, transform);
+    }
 }
 
