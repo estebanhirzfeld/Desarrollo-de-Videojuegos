b08ee3e934273c61ed99cbb61918551fe633f155
estebanhirzfeld <estebanhirzfeld@gmail.com>
2021-11-08 19:40:48 -0300

    intento de Squid Game

--- Hirzfeld/Assets/Scripts/CannonController.cs
+++ Hirzfeld/Assets/Scripts/CannonController.cs
@@ -5,22 +5,48 @@ using UnityEngine;
 public class CannonController : MonoBehaviour
 {
     public GameObject bulletPrefab;
+    public float fireFrequency = 10f;
+
+    enum Difficulties {Easy = 1, Normal, Hard};
+    [SerializeField] private Difficulties difficulty;
+
+
     // Start is called before the first frame update
     void Start()
     {
+    Debug.Log(difficulty);
+
+        switch (difficulty)
+        {
+            case Difficulties.Easy:
+                Debug.Log("MODO FACIL");
+                InvokeRepeating("SpawnBullet" ,4f, fireFrequency + 1f);   //Repeticion de Disparo con frecuencia variable desd Inspector
+                break;
+            case Difficulties.Normal:
+                Debug.Log("MODO INTERMEDIO");
+                InvokeRepeating("SpawnBullet" ,2f, fireFrequency - 1f);   //Repeticion de Disparo con frecuencia variable desd Inspector
+                break;
+            case Difficulties.Hard:
+                Debug.Log("MODO DIFICIL");
+                InvokeRepeating("SpawnBullet" ,1f, fireFrequency -0.9f);   //Repeticion de Disparo con frecuencia variable desd Inspector
+                break;
+            default:
+                Debug.Log("ERROR");
+                break;
+        }
+        
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))                //Con El Click Izquierdo El cannon Dispara una bola de Tenis
-        {
-            Debug.Log("Disparo!");                                              
-            Instantiate(bulletPrefab, transform);
-        }
+        
     }
 
-
-
+    void SpawnBullet()                                          //Funcion para instanciar el prefab de la bala
+    {
+        Instantiate(bulletPrefab, transform);
+    }
 }
 
--- Hirzfeld/Assets/Scripts/CapsuleMovement.cs
+++ Hirzfeld/Assets/Scripts/CapsuleMovement.cs
@@ -6,24 +6,82 @@ using System;
 public class CapsuleMovement : MonoBehaviour
 {
     public float playerSpeed;
+    float cameraAxis;
+
+    private float horizontalAxis;
+    private float verticalAxis;
+    private float rotationSpeed;
+    private bool enemyIsWatching;
+    private GameObject enemy;
+    private Vector3 enemyDirection;
     // Start is called before the first frame update
     void Start()
     {
-
+        enemy = GameObject.Find("Enemy");
 
     }
 
     // Update is called once per frame
     void Update()
     {
+        LookDirection();
         DirectionInputs();
+
     }
 
-    void DirectionInputs(){
+    private void DirectionInputs(){                                 //Movimiento de Poscicion del Player
         
-        float horizontalAxis = Input.GetAxis("Horizontal");
-        float verticalAxis = Input.GetAxis("Vertical");                 
+        horizontalAxis = Input.GetAxis("Horizontal");
+        verticalAxis = Input.GetAxis("Vertical");                 
 
-        transform.Translate(playerSpeed * Time.deltaTime * new Vector3(horizontalAxis, 0 , verticalAxis)); 
+        transform.Translate(playerSpeed * Time.deltaTime * new Vector3(horizontalAxis, 0 , verticalAxis));
     }
+
+    private void LookDirection(){                                   //Movimiento de camara del Player
+        cameraAxis += Input.GetAxis("Mouse X");
+        Quaternion angle = Quaternion.Euler(0,cameraAxis,0);
+        transform.localRotation = angle;    
+    }
+
+    private void TurnLook()
+    {
+        enemy.transform.rotation = Quaternion.LookRotation(new Vector3(enemyDirection.x, enemyDirection.y,180));    //El Enemigo Deja de Mirar hacia el player 
+        Debug.Log("Avanza");
+        enemyIsWatching = false;
+    }
+    
+    private void Look()
+    {
+        enemy.transform.rotation = Quaternion.LookRotation(new Vector3(enemyDirection.x, enemyDirection.y,-180));  //El Enemigo Blanco Mira hacia el player
+        enemyIsWatching = true;
+    }
+
+
+private void OnTriggerEnter(Collider other) {
+    if (other.tag == "CheckBox" || other.name == "StartLine")            //Si el Player pisa la linea de Inicio o el CheckBox invoca Look y espera 2 segundos
+    {
+        Look();
+        Debug.Log("Espera");
+        Invoke("TurnLook",2f);
+    }
+
+    if (enemyIsWatching == true && verticalAxis > 0.999)           //Si el Player se mueve muy rapido mientras el enemy esta mirando el juego termina
+    {
+        Debug.Log(verticalAxis);
+        Debug.Log("Dead");
+        UnityEditor.EditorApplication.isPlaying = false;
+    }
+
+    if (other.name == "EndLine")                                    //El Player al pisar la linea de meta Gana el Juego
+    {
+        transform.localScale = new Vector3(2,2,2);
+        Destroy (GameObject.FindWithTag("CheckBox"));
+        enemy.GetComponent<Collider>().isTrigger = false;
+
+
+
+        Debug.Log("Winner");
+    }
+}
+
 }
